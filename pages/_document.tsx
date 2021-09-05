df28f21612e0c3e12b7af9ae9dca3469c8537e7d
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-09-05 19:55:53 -0400

    added head logo:

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import { layout } from "@chakra-ui/react";
+import title from "next/head";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* <title>RU Hackerz</title> */}
+
+          <link rel="icon" sizes="16x16" href="../images/ruhackerz-logo.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import {
   UnorderedList,
   useDisclosure,
 } from "@chakra-ui/react";
+import link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import backgroundImg from "../images/background-ruhackerz.jpeg";
 import groupCodingImg from "../images/group-coding.jpeg";
@@ -32,6 +33,7 @@ import renderSocialMediaButtons from "../components/renderSocialMediaButtons";
 import sendMail from "../helpers/mailTo";
 import { FcGoogle } from "react-icons/fc";
 import { Bounce, Roll, Fade } from "react-reveal";
+import Head from "next/head";
 
 const IndexPage = () => {
   useEffect(() => {
@@ -64,258 +66,245 @@ const IndexPage = () => {
   }
 
   return (
-    <Box
-      bgImage={backgroundImg.src}
-      backgroundPosition="center"
-      bgSize="cover"
-      backgroundRepeat="no-repeat"
-      bgAttachment="fixed"
-      h="100%"
-    >
+    <>
+      <Head>
+        <link rel="icon" sizes="16x16" href="../images/ruhackerz-logo.ico" />
+        <title>RU Hackerz Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Box
-        ref={homePage}
-        id="body"
-        h="100vh"
-        w="100vw"
-        bgGradient="linear(to-b, rgb(0,0,0,.75),rgb(255,255,255,0))"
+        bgImage={backgroundImg.src}
+        backgroundPosition="center"
+        bgSize="cover"
+        backgroundRepeat="no-repeat"
+        bgAttachment="fixed"
+        h="100%"
       >
-        <Flex
-          justifyContent="flex-end"
-          alignItems="center"
-          id="nav-bar"
-          w="100%"
-          p={12}
-          h="10%"
-          ref={nav}
-          bg={showSticky && "rgb(0,0,0,0.55)"}
-          pos={process.browser && showSticky ? "fixed" : "unset"}
-          top="0px"
-          zIndex="999"
+        <Box
+          ref={homePage}
+          id="body"
+          h="100vh"
+          w="100vw"
+          bgGradient="linear(to-b, rgb(0,0,0,.75),rgb(255,255,255,0))"
         >
-          <Flex p={5}>
-            <NavLinks type={"Home"} button={homePage} />
-            <NavLinks type={"Our goal"} button={ourGoal} />
-            <NavLinks type={"About the team"} button={ourTeam} />
-            <NavLinks type={"Constitution & activities"} />
-          </Flex>
-        </Flex>
-        <Center h="80%" w="100%">
-          <Heading
-            fontFamily={theme.fonts.heading}
-            fontSize="6xl"
-            color="white"
-            bgGradient="linear(to-r, orange.500,blue.500)"
-            bgClip="text"
+          <Flex
+            justifyContent="flex-end"
+            alignItems="center"
+            id="nav-bar"
+            w="100%"
+            p={12}
+            h="10%"
+            ref={nav}
+            bg={showSticky && "rgb(0,0,0,0.55)"}
+            pos={process.browser && showSticky ? "fixed" : "unset"}
+            top="0px"
+            zIndex="999"
           >
-            RU Hackerz
-          </Heading>
-        </Center>
-        <Center h="10%" pb={10}>
-          <ScrollDown />
-        </Center>
-      </Box>
-      <Bounce right>
-        <Box id="our-goal" ref={ourGoal}>
-          <Center bg="gray.800" h="100%" w="100%" flexDir="column" pt={5}>
-            <Box>
-              <Heading
-                mt={5}
-                {...theme.headings}
-                borderBottom="1px solid white"
-              >
-                Our Goal
-              </Heading>
-            </Box>
-            <Flex maxW="85%">
-              <Box mt={3} ml={10} w="60%">
-                <Image src={groupCodingImg.src} />
+            <Flex p={5}>
+              <NavLinks type={"Home"} button={homePage} />
+              <NavLinks type={"Our goal"} button={ourGoal} />
+              <NavLinks type={"About the team"} button={ourTeam} />
+              <NavLinks type={"Constitution & activities"} />
+            </Flex>
+          </Flex>
+          <Center h="80%" w="100%">
+            <Heading
+              fontFamily={theme.fonts.heading}
+              fontSize="6xl"
+              color="white"
+              bgGradient="linear(to-r, orange.500,blue.500)"
+              bgClip="text"
+            >
+              RU Hackerz
+            </Heading>
+          </Center>
+          <Center h="10%" pb={10}>
+            <ScrollDown />
+          </Center>
+        </Box>
+        <Bounce right>
+          <Box id="our-goal" ref={ourGoal}>
+            <Center bg="gray.800" h="100%" w="100%" flexDir="column" pt={5}>
+              <Box>
+                <Heading
+                  mt={5}
+                  {...theme.headings}
+                  borderBottom="1px solid white"
+                >
+                  Our Goal
+                </Heading>
               </Box>
-              <Center w="100%" p="7.5%" textAlign="center">
-                <Text color="white" fontSize="lg">
-                  Here at RU Hackerz, our goal is to provide the perfect
-                  learning environment for students to collaborate and thrive in
-                  computer programming! Learning programming alone is hard - we
-                  believe that collaborative learning gives members a chance to
-                  demonstrate their understanding by helping fellow members
-                  solve a certain problem or even understand a certain concept.
-                </Text>
+              <Flex maxW="85%">
+                <Box mt={3} ml={10} w="60%">
+                  <Image src={groupCodingImg.src} />
+                </Box>
+                <Center w="100%" p="7.5%" textAlign="center">
+                  <Text color="white" fontSize="lg">
+                    Here at RU Hackerz, our goal is to provide the perfect
+                    learning environment for students to collaborate and thrive
+                    in computer programming! Learning programming alone is hard
+                    - we believe that collaborative learning gives members a
+                    chance to demonstrate their understanding by helping fellow
+                    members solve a certain problem or even understand a certain
+                    concept.
+                  </Text>
+                </Center>
+              </Flex>
+              <Flex maxW="85%">
+                <Center w="100%" p="7.5%" textAlign="center">
+                  <Text color="white" fontSize="lg">
+                    Build, Push, Deploy - This is our motto here at RU Hackerz.
+                    Our #1 objective is to have members hit the ground running
+                    by deploying several projects. We think that the best way of
+                    learning the sport is by playing the sport! Members will
+                    have used their acquired critical thinking and problem
+                    solving skills to ship projects that will disrupt the
+                    market.
+                  </Text>
+                </Center>
+                <Box ml={10} w="75%">
+                  <Image src={productHuntImg.src} />
+                </Box>
+              </Flex>
+            </Center>
+          </Box>
+        </Bounce>
+        <Box id="about-the-team" ref={ourTeam}>
+          <Center pt={5}>
+            <Heading {...theme.headings} borderBottom="1px solid white">
+              About the team
+            </Heading>
+          </Center>
+          <Roll left>
+            <Flex id="youssef-profile" h="90%" m="0 auto" p={20}>
+              <Box w="40%">
+                <Image
+                  //  filter="blur(20px)"
+                  src={myWashingtonPic.src}
+                />
+              </Box>
+              <Center m="0 auto" flexDir="column" w="50%">
+                <Box bg="gray.800" p={3}>
+                  <Flex>
+                    <Heading
+                      color="white"
+                      textAlign="center"
+                      mb={5}
+                      fontFamily={theme.fonts.heading}
+                    >
+                      Youssef El Mahallawy
+                    </Heading>
+                    {renderSocialMediaButtons(
+                      "youssefelmahallawy-348864203",
+                      "joeelmahallawy",
+                      "youssef-el-mahallawy"
+                    )}
+                  </Flex>
+                  <Flex>
+                    <Box m="0 auto auto 0">
+                      <Icon icon="emojione:waving-hand" fontSize="22.5" />
+                    </Box>
+
+                    <Text color="white" fontSize="md" ml={1}>
+                      {" "}
+                      Hey I'm Youssef, a third year software engineering
+                      student. I like solving problems and building projects! I
+                      learned Javascript over the summer and started building
+                      some simple projects for people to use,{" "}
+                      <Link
+                        _focus={{ outline: "none" }}
+                        color="blue.400"
+                        href="https://tzcities.vercel.app/"
+                        isExternal={true}
+                      >
+                        check it out!
+                      </Link>{" "}
+                      I'm excited for this group because I believe it's a great
+                      way to connect with and meet other amazing people who are
+                      interested in programming!
+                    </Text>
+                  </Flex>
+                </Box>
               </Center>
             </Flex>
-            <Flex maxW="85%">
-              <Center w="100%" p="7.5%" textAlign="center">
-                <Text color="white" fontSize="lg">
-                  Build, Push, Deploy - This is our motto here at RU Hackerz.
-                  Our #1 objective is to have members hit the ground running by
-                  deploying several projects. We think that the best way of
-                  learning the sport is by playing the sport! Members will have
-                  used their acquired critical thinking and problem solving
-                  skills to ship projects that will disrupt the market.
-                </Text>
+          </Roll>
+          {/* FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME: */}
+          <Roll right>
+            <Flex id="yasmin-profile" h="90%" m="0 auto" p={10} pr={40}>
+              <Center m="0 auto" flexDir="column" w="50%">
+                <Box bg="gray.800" p={3}>
+                  <Flex>
+                    <Heading
+                      color="white"
+                      textAlign="center"
+                      mb={5}
+                      fontFamily={theme.fonts.heading}
+                    >
+                      Yasmin Modarai
+                    </Heading>
+                    {renderSocialMediaButtons(
+                      "yasmin-modarai-8962b41b2",
+                      "fluff905"
+                    )}
+                  </Flex>
+                  <Flex>
+                    <Text color="white" fontSize="md" ml={1}>
+                      Hi I’m Yasmin Modarai and I’m going into my third year of
+                      Software Engineering. I joined RU Hackerz because I want
+                      to help others learn to program and build cool projects in
+                      a welcoming environment. A fun fact about me is that I’m
+                      currently a black belt in karate
+                    </Text>
+                  </Flex>
+                </Box>
               </Center>
-              <Box ml={10} w="75%">
-                <Image src={productHuntImg.src} />
+              <Box w="30%">
+                <Image src={YasminDP.src} />
+              </Box>
+            </Flex>
+          </Roll>
+        </Box>
+        <Fade bottom>
+          <Center h="50vh" id="sign-up">
+            <Button
+              size="lg"
+              colorScheme="orange"
+              fontSize="175%"
+              p="2.5%"
+              _hover={{ p: "2.75%", fontSize: "200%", bg: "orange.600" }}
+            >
+              <Link
+                _hover={{ borderBottom: "0px" }}
+                _focus={{ outline: "none" }}
+                isExternal={true}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSe33qTD56JENzFOfXbLgpuytILTjabY0ZF5UTcPcagr6nOmwQ/viewform"
+              >
+                Sign up!
+              </Link>
+            </Button>
+          </Center>
+        </Fade>
+        <Box w="100%" id="footer" bg="gray.900">
+          <Center h="100px">
+            <Flex gridGap="10">
+              <Box _hover={{ cursor: "pointer" }} onClick={sendMail}>
+                <FcGoogle size="40" />
+              </Box>
+              <Divider h="45px" colorScheme="gray" orientation="vertical" />
+              <Box h="100%">
+                <Link
+                  isExternal={true}
+                  href="https://www.instagram.com/ryersonhackerz/"
+                >
+                  <GrInstagram size="40" color="white" />
+                </Link>
               </Box>
             </Flex>
           </Center>
         </Box>
-      </Bounce>
-      <Box id="about-the-team" ref={ourTeam}>
-        <Center pt={5}>
-          <Heading {...theme.headings} borderBottom="1px solid white">
-            About the team
-          </Heading>
-        </Center>
-        <Roll left>
-          <Flex id="youssef-profile" h="90%" m="0 auto" p={20}>
-            <Box w="40%">
-              <Image
-                //  filter="blur(20px)"
-                src={myWashingtonPic.src}
-              />
-            </Box>
-            <Center m="0 auto" flexDir="column" w="50%">
-              <Box bg="gray.800" p={3}>
-                <Flex>
-                  <Heading
-                    color="white"
-                    textAlign="center"
-                    mb={5}
-                    fontFamily={theme.fonts.heading}
-                  >
-                    Youssef El Mahallawy
-                  </Heading>
-                  {renderSocialMediaButtons(
-                    "youssefelmahallawy-348864203",
-                    "joeelmahallawy",
-                    "youssef-el-mahallawy"
-                  )}
-                </Flex>
-                <Flex>
-                  <Box m="0 auto auto 0">
-                    <Icon icon="emojione:waving-hand" fontSize="22.5" />
-                  </Box>
-
-                  <Text color="white" fontSize="md" ml={1}>
-                    {" "}
-                    Hey I'm Youssef, a third year software engineering student.
-                    I like solving problems and building projects! I learned
-                    Javascript over the summer and started building some simple
-                    projects for people to use,{" "}
-                    <Link
-                      _focus={{ outline: "none" }}
-                      color="blue.400"
-                      href="https://tzcities.vercel.app/"
-                      isExternal={true}
-                    >
-                      check it out!
-                    </Link>{" "}
-                    I'm excited for this group because I believe it's a great
-                    way to connect with and meet other amazing people who are
-                    interested in proramming!
-                  </Text>
-                </Flex>
-              </Box>
-            </Center>
-          </Flex>
-        </Roll>
-        {/* FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME: */}
-        <Roll right>
-          <Flex id="yasmin-profile" h="90%" m="0 auto" p={10} pr={40}>
-            <Center m="0 auto" flexDir="column" w="50%">
-              <Box bg="gray.800" p={3}>
-                <Flex>
-                  <Heading
-                    color="white"
-                    textAlign="center"
-                    mb={5}
-                    fontFamily={theme.fonts.heading}
-                  >
-                    Yasmin Modarai
-                  </Heading>
-                  {renderSocialMediaButtons(
-                    "yasmin-modarai-8962b41b2",
-                    "fluff905"
-                  )}
-                </Flex>
-                <Flex>
-                  <Text color="white" fontSize="md" ml={1}>
-                    Hi I’m Yasmin Modarai and I’m going into my third year of
-                    Software Engineering. I joined RU Hackerz because I want to
-                    help others learn to program and build cool projects in a
-                    welcoming environment. A fun fact about me is that I’m
-                    currently a black belt in karate
-                  </Text>
-                </Flex>
-              </Box>
-            </Center>
-            <Box w="30%">
-              <Image src={YasminDP.src} />
-            </Box>
-          </Flex>
-        </Roll>
       </Box>
-      <Fade bottom>
-        <Center h="50vh" id="sign-up">
-          <Button
-            size="lg"
-            colorScheme="orange"
-            fontSize="175%"
-            p="2.5%"
-            _hover={{ p: "2.75%", fontSize: "200%", bg: "orange.600" }}
-          >
-            <Link
-              _hover={{ borderBottom: "0px" }}
-              _focus={{ outline: "none" }}
-              isExternal={true}
-              href="https://docs.google.com/forms/d/e/1FAIpQLSe33qTD56JENzFOfXbLgpuytILTjabY0ZF5UTcPcagr6nOmwQ/viewform"
-            >
-              Sign up!
-            </Link>
-          </Button>
-        </Center>
-      </Fade>
-      <Box w="100%" id="footer" bg="gray.900">
-        <Center h="100px">
-          <Flex gridGap="10">
-            <Box _hover={{ cursor: "pointer" }} onClick={sendMail}>
-              <FcGoogle size="40" />
-            </Box>
-            <Divider h="45px" colorScheme="gray" orientation="vertical" />
-            <Box h="100%">
-              <Link
-                isExternal={true}
-                href="https://www.instagram.com/ryersonhackerz/"
-              >
-                <GrInstagram size="40" color="white" />
-              </Link>
-            </Box>
-          </Flex>
-        </Center>
-      </Box>
-    </Box>
+    </>
   );
 };
 
 export default IndexPage;
-
-// function SlideFadeEx() {
-// const { isOpen, onToggle } = useDisclosure()
-
-//   return (
-//     <>
-//       <Button onClick={onToggle}>Click Me</Button>
-//       <SlideFade in={isOpen} offsetY="20px">
-//         <Box
-//           p="40px"
-//           color="white"
-//           mt="4"
-//           bg="teal.500"
-//           rounded="md"
-//           shadow="md"
-//         >
-//           <Lorem count={1} />
-//         </Box>
-//       </SlideFade>
-//     </>
-//   )
-// }
