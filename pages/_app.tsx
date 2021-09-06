42112b6d86222f2fa843038ed4ec77d057e1a938
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-09-06 01:47:22 -0400

    added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ class MyApp extends App {
   async componentDidMount() {
     if (process.env.NODE_ENV === "production") {
       ReactGA.initialize("UA-206784319-1");
-
       this.logPageView();
     }
   }
@@ -33,10 +32,6 @@ class MyApp extends App {
       <>
         <Head>
           <meta name="viewport" content="width=device-width" />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-5LBJ3L48MX"
-          ></script>
         </Head>
         <ChakraProvider theme={theme}>
           <DefaultSeo {...createSEOConfig()} />
